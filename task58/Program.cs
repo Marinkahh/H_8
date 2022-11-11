faaf570e1dc508f7bba084dcdbf6bdcd7fca7222
Marina <mod.1@bk.ru>
2022-11-11 21:32:48 +0400

    tack58

---
+++ task58/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// (посмотрите как реализуется произведение матриц, 
+// там не просто перемножение элемент на элемент)
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+void Main()
+{
+    int[,] A=GetArray(new int[2,2]);
+    int[,] B=GetArray(new int[2,2]);
+    int[,] C=MatrixProduct(A, B, new int[2,2]);
+    PrintArray(A);
+    Console.WriteLine();
+    PrintArray(B);
+    Console.WriteLine();
+    PrintArray(C);
+}
+
+int[,] GetArray(int[,] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MatrixProduct(int[,] A, int[,] B, int[,] C)
+{
+    for (int i = 0; i < A.GetLength(0); i++)
+    {
+        for (int j = 0; j < B.GetLength(1); j++)
+        {
+            for (int k = 0; k < B.GetLength(0); k++)
+            {
+                C[i, j] += A[i, k] * B[k, j];
+            }
+        }
+    }
+    return C;
+}
+Main();
+
